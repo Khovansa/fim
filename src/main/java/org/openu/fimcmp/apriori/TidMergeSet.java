cb602b972d636c948716c56f16fca8eeda8c8045
Alexander Khovansky <alexander.khovansky@gmail.com>
2016-11-12 22:15:34 +0000

    initial implementation of TidMergeSet

--- src/main/java/org/openu/fimcmp/apriori/TidMergeSet.java
+++ src/main/java/org/openu/fimcmp/apriori/TidMergeSet.java
@@ -1,5 +1,7 @@
 package org.openu.fimcmp.apriori;
 
+import scala.Tuple2;
+
 import java.io.Serializable;
 
 /**
@@ -33,15 +35,108 @@ public class TidMergeSet implements Serializable {
     private static final long INDEX_MASK = (-1L << REMAINDER_BITS_CNT); //1..1000000
     private static final long REMAINDER_MASK = (2L << REMAINDER_BITS_CNT)-1; //0..0111111
 
-    static long getIndex(long tid) {
-        return (tid & INDEX_MASK) >> REMAINDER_BITS_CNT;
+    private static final int RANK_IND = 0;
+    private static final int SIZE_IND = 1;
+    private static final int FIRST_ELEM_IND = 2;
+    private static final int LAST_ELEM_IND = 3;
+    private static final int AUXILIARY_FIELDS_CNT = 4; //rank, size, first, last
+
+    static long[] mergeElem(long[] tidSet, Tuple2<Integer, Long> rankAndTid, long totalTids) {
+        final int rank = rankAndTid._1;
+        final long tid = rankAndTid._2;
+
+        if (tidSet.length <= 1) {
+            return newSetWithElem(rank, tid, totalTids);
+        } else {
+            addElemToExistingSet(tidSet, tid);
+            return tidSet;
+        }
     }
 
-    static int getRemainder(long tid) {
-        return (int)(tid & REMAINDER_MASK);
+    static long[] mergeSets(long[] s1, long[] s2) {
+        if (s2.length <= 1) {
+            return s1;
+        }
+        if (s1.length <= 1) {
+            return copyOf(s2);
+        }
+
+        int currElemInd = (int)s2[FIRST_ELEM_IND];
+        do {
+            addBitsetToExistingSet(s1, currElemInd, s2[currElemInd]);
+            currElemInd = (int)s2[currElemInd + LONG_BITSETS_PER_PTR];
+        } while(currElemInd !=  0);
+        return s1;
     }
 
-    static long getRemainderAsBit(long tid) {
-        return 1<<(getRemainder(tid)-1);
+    private static long[] copyOf(long[] s2) {
+        long[] res = new long[s2.length];
+        System.arraycopy(s2, 0, res, 0, AUXILIARY_FIELDS_CNT);
+
+        //only go through the existing elements, not the entire array
+        int currElemInd = (int)s2[FIRST_ELEM_IND];
+        do {
+            System.arraycopy(s2, currElemInd, res, currElemInd, LONG_BITSETS_PER_PTR+1);
+            currElemInd = (int)s2[currElemInd + LONG_BITSETS_PER_PTR];
+        } while(currElemInd !=  0);
+        return res;
     }
+
+    private static long[] newSetWithElem(int rank, long tid, long totalTids) {
+        if (tid < 0) {
+            //the case when the itemset (rank) is present in this transaction (TID),
+            // but we only aggregate those that are not present
+            return new long[]{rank};  //'zero' TID set for this rank
+        }
+
+        long[] res = new long[getArrayLen(totalTids)];
+        res[RANK_IND] = rank;
+        res[SIZE_IND] = 1;
+        int index = getIndex(tid);
+        res[FIRST_ELEM_IND] = res[LAST_ELEM_IND] = index;
+        res[index] = getRemainderAsBit(tid);
+        return res;
+    }
+
+    private static void addElemToExistingSet(long[] tidSet, long tid) {
+        if (tid < 0) {
+            //the case when the itemset (rank) is present in this transaction (TID),
+            // but we only aggregate those that are not present
+            return; //nothing to do
+        }
+
+        int newIndex = getIndex(tid);
+        long bitSet = getRemainderAsBit(tid);
+        addBitsetToExistingSet(tidSet, newIndex, bitSet);
+    }
+
+    private static void addBitsetToExistingSet(long[] tidSet, int newIndex, long bitSet) {
+        if (tidSet[newIndex] != 0) {
+            tidSet[newIndex] = tidSet[newIndex] | bitSet;
+        } else {
+            //a new element
+            tidSet[newIndex] = bitSet;
+            ++tidSet[SIZE_IND];
+            int lastElemPtr = (int) tidSet[LAST_ELEM_IND] + LONG_BITSETS_PER_PTR;
+            tidSet[LAST_ELEM_IND] = tidSet[lastElemPtr] = newIndex;
+        }
+    }
+
+    //TODO: if LONG_BITSETS_PER_PTR > 1, need to return 2 numbers
+    private static int getIndex(long tid) {
+        int indexInPureBitSet = (int) ((tid & INDEX_MASK) >> REMAINDER_BITS_CNT);
+        return AUXILIARY_FIELDS_CNT + (LONG_BITSETS_PER_PTR + 1) * indexInPureBitSet;
+    }
+
+    private static long getRemainderAsBit(long tid) {
+        int remainder = (int)(tid & REMAINDER_MASK);
+        return 1L<<remainder;
+    }
+
+    private static int getArrayLen(long totalTids) {
+        int longBitSetCnt = (int)Math.ceil(1.0 * totalTids / 64);
+        int ptrCnt = (int)Math.ceil(1.0 * longBitSetCnt / LONG_BITSETS_PER_PTR);
+        return AUXILIARY_FIELDS_CNT + longBitSetCnt + ptrCnt;
+    }
+
 }
--- src/test/java/org/openu/fimcmp/apriori/TidMergeSetTest.java
+++ src/test/java/org/openu/fimcmp/apriori/TidMergeSetTest.java
@@ -1,17 +1,45 @@
 package org.openu.fimcmp.apriori;
 
 import org.junit.Test;
+import scala.Tuple2;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class TidMergeSetTest {
     @Test
     public void tmp() {
-        System.out.println(String.format("%s", Long.toBinaryString(-1L)));
-        System.out.println(String.format("%s", Long.toBinaryString((2L<<6)-1)));
-        System.out.println(String.format("%s", TidMergeSet.getIndex(131L))); //receiving index by input number
-        System.out.println(String.format("%s", TidMergeSet.getRemainder(131L))); //remainder
-        System.out.println(String.format("%s", Long.toBinaryString(TidMergeSet.getRemainderAsBit(131L)))); //bit set
+        final long totalTids = 180L;
+        final int rank = 17;
+        long[] res = TidMergeSet.mergeElem(new long[]{}, new Tuple2<>(rank, 23L), totalTids);
+        res = TidMergeSet.mergeElem(res, new Tuple2<>(rank, 0L), totalTids);
+        res = TidMergeSet.mergeElem(res, new Tuple2<>(rank, 2L), totalTids);
+        res = TidMergeSet.mergeElem(res, new Tuple2<>(rank, 5L), totalTids);
+        res = TidMergeSet.mergeElem(res, new Tuple2<>(rank, 179L), totalTids);
+        res = TidMergeSet.mergeElem(res, new Tuple2<>(rank, 129L), totalTids);
+        res = TidMergeSet.mergeElem(res, new Tuple2<>(rank, 128L), totalTids);
+
+        long[] res2 = TidMergeSet.mergeElem(new long[]{}, new Tuple2<>(rank, 23L), totalTids);
+        res2 = TidMergeSet.mergeElem(res2, new Tuple2<>(rank, 64L), totalTids);
+        res2 = TidMergeSet.mergeElem(res2, new Tuple2<>(rank, 65L), totalTids);
+        res2 = TidMergeSet.mergeElem(res2, new Tuple2<>(rank, 66L), totalTids);
+        res2 = TidMergeSet.mergeElem(res2, new Tuple2<>(rank, 127L), totalTids);
+        System.out.println("s1:");
+        System.out.println(Arrays.toString(res));
+        System.out.println(Long.toBinaryString(res[(int)res[2]]));
+        System.out.println(Long.toBinaryString(res[(int)res[3]]));
+        System.out.println("s2:");
+        System.out.println(Arrays.toString(res2));
+        System.out.println(Long.toBinaryString(res2[(int)res2[2]]));
+        System.out.println(Long.toBinaryString(res2[(int)res2[3]]));
+
+        res = TidMergeSet.mergeSets(res, res2);
+        res = TidMergeSet.mergeSets(new long[]{}, res);
+        System.out.println("After merge:");
+        System.out.println(Arrays.toString(res));
+        System.out.println(Long.toBinaryString(res[(int)res[2]]));
+        System.out.println(Long.toBinaryString(res[(int)res[3]]));
     }
 
 }
