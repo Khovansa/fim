80c0b1d55e828ca12d8c132cd659508534a65132
Alexander Khovansky <alexander.khovansky@gmail.com>
2016-11-18 21:51:15 +0000

    Add BitArrays class + test

---
+++ src/main/java/org/openu/fimcmp/util/BitArrays.java
@@ -0,0 +1,61 @@
+package org.openu.fimcmp.util;
+
+/**
+ * Common bit-manipulation methods on long[]. <br/>
+ * Designed to be as fast as possible, assumes all arguments are correct. <br/>
+ */
+public class BitArrays {
+    private final static int ADDRESS_BITS_PER_WORD = 6;
+    private final static int BITS_PER_WORD = (1<<ADDRESS_BITS_PER_WORD);
+
+    public static void set(long[] words, int bitSetStartInd, int bitIndex) {
+        int wordIndex = wordIndex(bitSetStartInd, bitIndex);
+        words[wordIndex] |= asBit(bitIndex);
+    }
+
+    public static boolean get(long[] words, int bitSetStartInd, int bitIndex) {
+        int wordIndex = wordIndex(bitSetStartInd, bitIndex);
+        return ((words[wordIndex] & (1L << bitIndex)) != 0);
+
+    }
+
+    public static int cardinality(long[] words, int bitSetStartInd) {
+        int sum = 0;
+        for (int i = bitSetStartInd; i < words.length; i++) {
+            sum += Long.bitCount(words[i]);
+        }
+        return sum;
+    }
+
+    public static int[] asNumbers(long[] words, int bitSetStartInd) {
+        int[] res = new int[cardinality(words, bitSetStartInd)];
+        int resInd = 0;
+        for (int wordIndex = bitSetStartInd; wordIndex < words.length; ++wordIndex) {
+            long word = words[wordIndex];
+            if (word != 0) {
+                int base = (wordIndex - bitSetStartInd) * BITS_PER_WORD;
+                resInd = addWordBitsAsNumbers(res, resInd, base, word);
+            }
+        }
+        return res;
+    }
+
+    private static int addWordBitsAsNumbers(int[] res, int resInd, int base, long word) {
+        long currWord = word;
+        while (currWord != 0) {
+            int bitIndex = Long.numberOfTrailingZeros(currWord);
+            res[resInd++] = base + bitIndex;
+            currWord = currWord & ~(asBit(bitIndex));
+        }
+        return resInd;
+    }
+
+
+    private static int wordIndex(int bitSetStartInd, int bitIndex) {
+        return bitSetStartInd + (bitIndex >> ADDRESS_BITS_PER_WORD);
+    }
+
+    private static long asBit(int bitIndex) {
+        return 1L << bitIndex;
+    }
+}
---
+++ src/test/java/org/openu/fimcmp/util/BitArraysTest.java
@@ -0,0 +1,41 @@
+package org.openu.fimcmp.util;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class BitArraysTest {
+    private static final int START_IND=3;
+
+    @Test
+    public void test() {
+        long[] words = new long[24];
+        int[] indexes = {0, 1, 7, 63, 64, 65, 128, 192, 200, 213, 214, 1034, 1234};
+        for (int bitIndex : indexes) {
+            BitArrays.set(words, START_IND, bitIndex);
+        }
+
+        for (int bitIndex : indexes) {
+            assertTrue(""+bitIndex, BitArrays.get(words, START_IND, bitIndex));
+        }
+        assertFalse(BitArrays.get(words, START_IND, 2));
+        assertFalse(BitArrays.get(words, START_IND, 8));
+        assertFalse(BitArrays.get(words, START_IND, 100));
+
+        assertEquals(indexes.length, BitArrays.cardinality(words, START_IND));
+
+
+        List<Integer> nums = asList(BitArrays.asNumbers(words, START_IND));
+        assertThat(nums, is(asList(indexes)));
+    }
+
+    private static List<Integer> asList(int[] nums) {
+        return Arrays.asList(ArrayUtils.toObject(nums));
+    }
+}
